1dd94a5f1d37ffa24fd8af31910130adcab72643
Yannnick <48526635+Yannnick@users.noreply.github.com>
2019-03-15 13:08:52 +0100

    Add files via upload

--- search.py
+++ search.py
@@ -9,31 +9,32 @@ temp.shuffle(n)# shuffeling the goal node
 start = node.Node(None,temp,0)# creating a neu node with a shuffled puzzle
 
 
-def min():
+def min(fronteer):
 	position=0
-	min=global.fronteer[0].f
-	for i in xrange(1, len(global.fronteer)):
-		if(global.fronteer[i].f<min):
-			min=global.fronteer[i].f
+	min=fronteer[0].f
+	for i in range(1, len(fronteer)):
+		if(fronteer[i].f<min):
+			min=fronteer[i].f
 			position=i
 	return position
 
 def exist(list, element):
 	temp = len(list)
-	for x in xrange(len(list)):
+	for i in range(len(list)):
 		if list[i]==element and list[i].f > element.f:
 			temp = i
 	return temp
 
-def search(self, start, h):
+def search(start, h):
 	fronteer = list()
 	visited = list()
+	solution = list()
 	temp = node.State(size)# creating a solved puzzle
 	goal = node.Node(None,temp,0)# creating a goal node
 	fronteer.append(start) # add the first state to the fronteer
 	foundIt = False
 	while len(fronteer)!=0 and foundIt==False:
-		aVisiter = fronteer.pop(global.min())
+		aVisiter = fronteer.pop(min(fronteer))
 		nEtats = aVisiter.expanseH(h)
 		while len(nEtats)!=0:
 			n = nEtats.pop()
@@ -51,3 +52,12 @@ def search(self, start, h):
 					foundIt = True
 				else: 
 					fronteer.append(n)
+	temp=goal.father
+	while temp.father != None:
+		solution.append(0,temp) 
+		temp = temp.father
+	return solution
+
+t = search(start,h)
+for i in range (len(t)):
+	print(t[i].son.puzzle)
